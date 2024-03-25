75713e8d95e4a19dc91574411dcdc4ddebea7903
AlecadM <71269820+AlexanderMeynik@users.noreply.github.com>
2024-03-25 22:11:41 +0300

    Move lib,add tests integration

---
+++ Google_tests/test1.cpp
@@ -0,0 +1,20 @@
+#include "../application/lib.h"
+#include <gtest/gtest.h>
+//https://stackoverflow.com/questions/6759560/boosttest-and-mocking-framework
+
+TEST(integarion_test,test_integr)
+{
+    auto res= integrate<double>([](double x)->double{return 2*x;},0,2);
+    ASSERT_EQ(res,4);
+}
+TEST(integarion_test,test_integr2)
+{
+    auto res= integrate<double>([](double x)->double{return 2*x;},0,1);
+    ASSERT_EQ(res,1);
+}
+
+TEST(integarion_test,test_integr3)
+{
+    auto res= integrate<double>([](double x)->double{return 2*x;},0,3);
+    ASSERT_EQ(res,9);
+}
---
+++ application/main.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include "Dipoles.h"
+#include <vector>
+#include <algorithm>
+#include <numeric>
+#include <ostream>
+#include <fstream>
+#include <iomanip>
+#include <filesystem>
+#include <boost/math/quadrature/gauss_kronrod.hpp>
+#include <matplot/matplot.h>
+#include "lib.h"
+
+
+const std::size_t maxPrecision = std::numeric_limits<double>::digits;
+
+
+using  namespace dipoles;
+const double  l=1E-7;
+int main(int argc, char* argv[]) {
+    std::ios_base::sync_with_stdio(false);
+    int verbose=2;
+    std::string filename="config.txt";
+    if(argc==2)
+    {
+        filename=argv[1];
+    }
+    if(argc==3)
+    {
+        char* end;
+        verbose=std::strtol(argv[2],&end,10);
+    }
+    auto avec= parseConf<double>(filename);
+    std::string dirname=filename.erase(filename.find('.'));
+    std::filesystem::create_directory(dirname);
+    for (int i = 0; i < avec.size(); ++i) {
+        std::vector<Eigen::Vector<double,2>>a=avec[i];
+        int N=a.size();
+        Dipoles< double> d(N,a);
+        d.solve_();
+        auto solut2=d.getSolution_();
+        printToFile<double>(N, a, d, dirname,i,verbose);
+    }
+
+    return 0;
+}
+
+
+
+
+
+
+
+
