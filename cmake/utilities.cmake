0dbf634a0af7a458b210886d0de032c22c4c317c
AlecadM <71269820+AlexanderMeynik@users.noreply.github.com>
2025-05-20 17:08:33 +0300

    Use more flexible checks for directory and packages presence.

--- cmake/utilities.cmake
+++ cmake/utilities.cmake
@@ -38,7 +38,7 @@ endfunction()
 macro(add_env_test TARGET_NAME SOURCE_FILE ARGN)
     set(_missing "")
     set(_args     "")
-    message(STATUS "Start test ${TARGET_NAME} configuration "
+    message(STATUS "Start test ${TARGET_NAME} configuration  with"
             "required ENVIROMENT variables: ${${ARGN}}")
     foreach(_var IN LISTS ${ARGN})
         if (NOT DEFINED ENV{${_var}})
@@ -71,5 +71,67 @@ macro(add_env_test TARGET_NAME SOURCE_FILE ARGN)
         )
 
         message(STATUS "Configured test ${TARGET_NAME} with args: ${_args_list}")
+    endif()
+endmacro()
+
+# -----------------------------------------------------------------------------
+# add_subdirectory_with_deps(<subdir> <DEP_LIST_VAR> <DIR_LIST_VAR>)
+#     <PKG_LIST_VAR>     # a CMake variable name holding a list of package names
+#     <DIR_LIST_VAR>     # a CMake variable name holding a list of subdir names
+# )
+#
+# Checks:
+#  - for each pkg in ${${PKG_LIST_VAR}}, that <pkg>_FOUND is true
+#  - for each dir in ${${DIR_LIST_VAR}}, that it’s been added earlier
+#
+# If *all* checks pass, calls add_subdirectory(<subdir>) and records it in
+# ADDED_SUBDIRECTORIES.  Otherwise emits a WARNING and skips.
+# -----------------------------------------------------------------------------
+macro(add_subdirectory_with_deps SUBDIR DEP_LIST_VAR DIR_LIST_VAR)
+    set(_deps ${${DEP_LIST_VAR}})
+
+    set(_dirs ${${DIR_LIST_VAR}})
+
+    set(_missing "")
+    message(STATUS "Start adding '${SUBDIR}' with required "
+            "package(s): ${_deps} and subdir(s): ${_dirs}")
+
+    if (NOT DEFINED ADDED_SUBDIRECTORIES)
+        set(ADDED_SUBDIRECTORIES "")
+    endif()
+
+    foreach(_pkg IN LISTS _deps)
+        if (NOT ${_pkg}_FOUND)
+            list(APPEND _missing ${_pkg})
+        endif()
+    endforeach()
+
+    set(_missing_dirs "")
+    foreach(_req_dir IN LISTS _dirs)
+        list(FIND ADDED_SUBDIRECTORIES ${_req_dir} _idx)
+        if (_idx EQUAL -1)
+            list(APPEND _missing_dirs ${_req_dir})
+        endif()
+    endforeach()
+
+
+    if (_missing OR _missing_dirs)
+        if (_missing_pkgs)
+        list(JOIN _missing ", " _miss_joined)
+        set(_msg_pkg " missing package(s): ${_mp};")
+        endif()
+        if (_missing_dirs)
+            list(JOIN _missing_dirs ", " _md)
+            set(_msg_dir " missing subdir(s): ${_md};")
+        endif()
+
+        message(WARNING
+                "Skipping add_subdirectory(${SUBDIR}):"
+                "${_msg_pkg}${_msg_dir}"
+        )
+    else()
+        message(STATUS "Adding '${SUBDIR}'.")
+        add_subdirectory(${SUBDIR})
+        list(APPEND ADDED_SUBDIRECTORIES ${SUBDIR})
     endif()
 endmacro()
